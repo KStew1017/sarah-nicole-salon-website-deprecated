1bd076da42c1a60118ce38e5c1de0cf3d9719b87
Kyle Stewart <kstew1017@outlook.com>
2023-04-30 17:39:58 -0500

    created navbar and hero section

---
+++ js/script.js
@@ -0,0 +1,41 @@
+document.querySelector('.hamburger-button').addEventListener('click', function () {
+
+    document.querySelector('#hamburger').classList.toggle('open');
+});
+
+
+
+$(window).scroll(function() {
+    $('.fade-in').each(function() {
+        var position = $(this).offset().top;
+        var scroll = $(window).scrollTop();
+        var windowHeight = $(window).height();
+        if (scroll > position - windowHeight + 200) {
+            $(this).addClass('visible');
+        }
+    });
+});
+
+
+
+window.addEventListener('load', function() {
+    var heroElements = document.querySelectorAll('.card-animation');
+    heroElements.forEach(function(element) {
+        element.classList.remove('animate-card');
+        setTimeout(function() {
+            element.classList.add('animate-card');
+        }, 100);
+    });
+});
+
+
+
+window.addEventListener('load', function() {
+    var heroElements = document.querySelectorAll('.img-animation');
+    heroElements.forEach(function(element) {
+        element.classList.remove('animate-img');
+        setTimeout(function() {
+            element.classList.add('animate-img');
+        }, 100);
+    });
+});
