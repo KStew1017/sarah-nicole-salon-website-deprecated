33e7add56f270941cd9066679c9fbaf43d00c8d6
Kyle Stewart <kstew1017@outlook.com>
2023-05-10 15:21:23 -0500

    added carousel to hero and animations

--- js/script.js
+++ js/script.js
@@ -1,57 +1,67 @@
-document.querySelector('.hamburger-button').addEventListener('click', function () {
+// history.scrollRestoration = 'manual';
 
-    document.querySelector('#hamburger').classList.toggle('open');
+// document.querySelector('.hamburger-button').addEventListener('click', function () {
+//     document.querySelector('#hamburger').classList.toggle('open');
+// });
+
+// const cards = document.querySelectorAll(`
+//     .stylist-card-odd, 
+//     .stylist-card-even, 
+//     .hero-image, 
+//     .hero-card,
+//     .section-title h1
+// `);
+
+// const observer = new IntersectionObserver(
+//     entries => {
+//         entries.forEach(entry => {
+//             entry.target.classList.toggle("show", entry.isIntersecting);
+//             if (entry.isIntersecting) observer.unobserve(entry.target);
+//         })
+//     },
+//     {
+//         threshold: 0.8
+//     }
+// );
+
+// cards.forEach(card => {
+//     observer.observe(card);
+// });
+
+history.scrollRestoration = 'manual';
+
+$('.hamburger-button').click(function () {
+    $('#hamburger').toggleClass('open');
 });
 
+const cards = $(`
+    .stylist-card-odd, 
+    .stylist-card-even, 
+    .carousel, 
+    .hero-card, 
+    .section-title h1
+`);
 
+const observer = new IntersectionObserver(
+    entries => {
+        entries.forEach(entry => {
+            $(entry.target).toggleClass("show", entry.isIntersecting);
+            if (entry.isIntersecting) observer.unobserve(entry.target);
+        })
+    },
+    { threshold: 0.8 }
+);
 
-$(window).scroll(function() {
-    $('.fade-in').each(function() {
-        var position = $(this).offset().top;
-        var scroll = $(window).scrollTop();
-        var windowHeight = $(window).height();
-        if (scroll > position - windowHeight + 200) {
-            $(this).addClass('visible');
-        }
-    });
+cards.each(function(index, card) {
+    observer.observe(card);
 });
 
+function navbarScroll() {
+    if (window.scrollY > 0) {
+        $('.navbar').addClass('scrolled');
+    } else {
+        $('.navbar').removeClass('scrolled');
+    }
+}
 
-
-window.addEventListener('load', function() {
-    var heroElements = document.querySelectorAll('.card-animation');
-    heroElements.forEach(function(element) {
-        element.classList.remove('animate-card');
-        setTimeout(function() {
-            element.classList.add('animate-card');
-        }, 100);
-    });
-});
-
-
-
-window.addEventListener('load', function() {
-    var heroElements = document.querySelectorAll('.img-animation');
-    heroElements.forEach(function(element) {
-        element.classList.remove('animate-img');
-        setTimeout(function() {
-            element.classList.add('animate-img');
-        }, 100);
-    });
-});
-
-
-
-window.addEventListener('load', function() {
-    var heroElements = document.querySelectorAll('.text-animation');
-    heroElements.forEach(function(element) {
-        element.classList.remove('animate-text');
-        setTimeout(function() {
-            element.classList.add('animate-text');
-        }, 100);
-    });
-});
-
-const scrollText = document.querySelector('.scroll-text');
-const scrollTextClone = scrollText.cloneNode(true);
-scrollText.parentNode.appendChild(scrollTextClone);
+window.addEventListener('scroll', navbarScroll);
