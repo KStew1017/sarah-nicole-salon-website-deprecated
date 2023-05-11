6058f57ccba6ad8d54f0e58f38be21d7635d18dd
Kyle Stewart <kstew1017@outlook.com>
2023-05-10 19:48:09 -0500

    added responsiveness and styling

--- js/script.js
+++ js/script.js
@@ -1,67 +1,63 @@
-// history.scrollRestoration = 'manual';
 
-// document.querySelector('.hamburger-button').addEventListener('click', function () {
-//     document.querySelector('#hamburger').classList.toggle('open');
-// });
+$(function () {
 
-// const cards = document.querySelectorAll(`
-//     .stylist-card-odd, 
-//     .stylist-card-even, 
-//     .hero-image, 
-//     .hero-card,
-//     .section-title h1
-// `);
-
-// const observer = new IntersectionObserver(
-//     entries => {
-//         entries.forEach(entry => {
-//             entry.target.classList.toggle("show", entry.isIntersecting);
-//             if (entry.isIntersecting) observer.unobserve(entry.target);
-//         })
-//     },
-//     {
-//         threshold: 0.8
-//     }
-// );
-
-// cards.forEach(card => {
-//     observer.observe(card);
-// });
-
-history.scrollRestoration = 'manual';
-
-$('.hamburger-button').click(function () {
-    $('#hamburger').toggleClass('open');
-});
-
-const cards = $(`
-    .stylist-card-odd, 
-    .stylist-card-even, 
-    .carousel, 
-    .hero-card, 
-    .section-title h1
-`);
-
-const observer = new IntersectionObserver(
-    entries => {
-        entries.forEach(entry => {
-            $(entry.target).toggleClass("show", entry.isIntersecting);
-            if (entry.isIntersecting) observer.unobserve(entry.target);
-        })
-    },
-    { threshold: 0.8 }
-);
-
-cards.each(function(index, card) {
-    observer.observe(card);
-});
-
-function navbarScroll() {
-    if (window.scrollY > 0) {
-        $('.navbar').addClass('scrolled');
+//---------- Reset scroll position on page refresh -----------//
+    history.scrollRestoration = 'manual';
+    
+//---------- Hamburger menu open -----------//
+    $('.hamburger-button').click(function () {
+        $('#hamburger').toggleClass('open');
+    });
+    
+//---------- Intersection observer for fade in effect on first time viewed -----------//
+    if ($(window).width() > 768) {
+        const cards = $(`
+            .stylist-card-odd, 
+            .stylist-card-even, 
+            .carousel, 
+            .hero-card, 
+            .section-title h1
+        `);
+        
+        const observer = new IntersectionObserver(
+            entries => {
+                entries.forEach(entry => {
+                    $(entry.target).toggleClass("show", entry.isIntersecting);
+                    if (entry.isIntersecting) observer.unobserve(entry.target);
+                })
+            },
+            { threshold: 0.8 }
+        );
+        
+        cards.each(function(index, card) {
+            observer.observe(card);
+        });
     } else {
-        $('.navbar').removeClass('scrolled');
+        $(`
+        .stylist-card-odd, 
+        .stylist-card-even, 
+        .carousel, 
+        .hero-card, 
+        .section-title h1
+        `).addClass('show');
     }
-}
+    
+//---------- Navbar fade in effect on scroll -----------//
+    function navbarScroll() {
+        if (window.scrollY > 0) {
+            $('.navbar').addClass('scrolled');
+        } else {
+            $('.navbar').removeClass('scrolled');
+        }
+    }
 
-window.addEventListener('scroll', navbarScroll);
+    window.addEventListener('scroll', navbarScroll);
+
+//---------- Testimonials marquee scroll -----------//
+    $('.marquee').marquee({
+        duration: 10000,
+        gap: 100,
+        duplicated: true,
+    });
+})
+
