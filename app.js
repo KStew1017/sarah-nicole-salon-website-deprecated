a96f32b33ac74c2c5d4ecb5be308cb7bcef9417a
Kyle Stewart <kstew1017@outlook.com>
2023-05-28 12:10:44 -0500

    modified file structure

--- app.js
+++ app.js
@@ -14,4 +14,7 @@ app.get('/stylists', (req, res) => {
     res.status(403).render('error');
 });
 
-app.listen(3000);
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log('Server running successfully');
+});
