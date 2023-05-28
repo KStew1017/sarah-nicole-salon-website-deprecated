a19388b552fa51cb9758c533280d21f125bf6608
Kyle Stewart <kstew1017@outlook.com>
2023-05-28 14:40:58 -0500

    removed file extensions from urls

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const path = require('path');
 
 router.get('/', function(req, res, next) {
-  res.sendFile(path.join(__dirname, 'public/index.html'));
+  res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 module.exports = router;
